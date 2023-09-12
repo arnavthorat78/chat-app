8b3c2351554f11f323e8a69fad3a83b5e0c13580
arnavthorat78 <arnavthorat78@gmail.com>
2023-09-12 16:45:50 +1000

    Added styles and improved date formatting

--- src/ui.js
+++ src/ui.js
@@ -12,11 +12,15 @@ class ChatUI {
    * @param {{ username: string, message: string, created_at: Date }} data Data for outputting to the DOM.
    */
   render(data) {
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true,
+    });
+
     const html = `
       <li class="list-group-item">
-        <span class="username">${data.username}</span>
+        <span class="username">${data.username}:</span>
         <span class="message">${data.message}</span>
-        <div class="time">${data.created_at.toDate()}</div>
+        <div class="time">${when}</div>
       </li>
     `;
 
